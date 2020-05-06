d24964f1cf99de72832abebd535742204a9ce1ea
Diego Kamiha <diego.kamiha@ifood.com.br>
2020-05-06 17:03:12 -0300

    Initial Commit

---
+++ covid-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  covid-app
+//
+//  Created by Diego Kamiha  on 06/05/20.
+//  Copyright © 2020 kamiha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
