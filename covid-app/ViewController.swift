10fa7e9555e5a4fc2c9d0d56c8ba49ba14dcf5a5
Diego Kamiha <diego.kamiha@ifood.com.br>
2020-05-07 16:56:02 -0300

    Learning some IB<somethign> stuff (Im stivi jobis)

--- covid-app/ViewController.swift
+++ covid-app/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UITextView!
+    
+    @IBOutlet weak var number: UITextView!
+    
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onUpdateClicked(_ sender: Any) {
+        count += 1
+        number.text = String(count)
+    }
 }
-
